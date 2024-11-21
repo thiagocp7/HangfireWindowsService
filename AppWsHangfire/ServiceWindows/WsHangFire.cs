fa2458b5b697ae6d7128d1d7dc99384db8036778
Thiago C Pereira <tcpereira@iportsolutions.com.br>
2024-11-21 16:03:09 -0300

    Consegui rodar o Hangfire e adicionar a interface Web

--- AppWsHangfire/ServiceWindows/WsHangFire.cs
+++ AppWsHangfire/ServiceWindows/WsHangFire.cs
@@ -9,31 +9,55 @@ using System.Text;
 using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.SqlServer;
+using Microsoft.Owin.Hosting;
 
 namespace AppWsHangfire.ServiceWindows
 {
     partial class WsHangFire : ServiceBase
     {
+        private IDisposable _webApp;
         private BackgroundJobServer _server;
 
         public WsHangFire()
         {
             InitializeComponent();
 
-            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireConnection");
+            // GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireConnection");
+
+            GlobalConfiguration.Configuration
+                               .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                               .UseSimpleAssemblyNameTypeSerializer()
+                               .UseRecommendedSerializerSettings()
+                               .UseSqlServerStorage(@"Data Source=BR-NOT-DEV-19-B\SQLEXPRESS;Initial Catalog=bdHangfire;User ID=hangfire;Password=654321;Encrypt=False");
+
         }
 
         protected override void OnStart(string[] args)
         {
+
+            string baseAddress = "http://localhost:5000/"; // Porta e endereço configuráveis
+            _webApp = WebApp.Start<Startup>(baseAddress);
+
+
+            BackgroundJob.Enqueue(() => testeErro()); 
+
             // TODO: Adicione aqui o código para iniciar seu serviço.
             _server = new BackgroundJobServer();
-            _server.Start();
+           
         }
 
         protected override void OnStop()
         {
             // TODO: Adicione aqui o código para realizar qualquer desmontagem necessária para interromper seu serviço.
-            _server.Dispose();
+            _server.Dispose(); // Para o servidor Hangfire
+            _webApp?.Dispose(); // Para o servidor web (SelfHost)
+            EventLog.WriteEntry("OWIN parado.", EventLogEntryType.Information);
+        }
+
+        public static void testeErro() 
+        {
+            throw new Exception("Teste de erro!");
+            System.IO.File.WriteAllText("C:\\Dev\\testeHangfire.txt", "Rodor o hangfire");
         }
 
         public void OnStartTeste() 
---
+++ AppWsHangfire/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+namespace AppWsHangfire
+{
+   
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configurar o painel do Hangfire
+            app.UseHangfireDashboard("/hangfire");
+
+            app.Run(context =>
+            {
+                // Retorna uma resposta simples
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Servidor OWIN rodando no Serviço Windows!");
+            });
+        }
+    }
+}
